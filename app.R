3a333fbc992cc67c414e0ea665129d2ea5c07523
Toby Weed <tobyweed@gmail.com>
2024-01-19 15:43:02 -0500

    add facility homepages

--- app.R
+++ app.R
@@ -17,7 +17,7 @@ max_date = max(append(facs$start_date, missiles$start_date), na.rm = TRUE)
 
 
 ## CLIENT
-ui <- fluidPage(
+ui <- navbarPage("Nuclear Recon Explorer",
   # custom styling
   tags$style(type = "text/css", "
         .js-irs-0 .irs-single, .js-irs-0 .irs-bar-edge, .js-irs-0 .irs-bar { }
@@ -27,39 +27,66 @@ ui <- fluidPage(
         .subtitle { padding-left: 15px; 
                     color: #808080; }
       "),
-  titlePanel("Nuclear Recon Explorer"),
-  fluidRow(
-    h3("Explore U.S. Satellite Reconnaissance of Nuclear Targets from 1941-1985", class = "subtitle"),
-    br()
-  ),
-  sidebarLayout(
-    sidebarPanel(
-      checkboxInput("showUnknown","Show Facilities with Unknown Start Dates", value = FALSE),
-      checkboxInput("showCaptures","Show Spotted Facilities and Missiles", value = FALSE),
-      sliderInput("dateSlider", "Adjust Date:", 
-                  min = min_date, 
-                  max = max_date, 
-                  value = min_date, 
-                  step = 49)
-                  # animate = animationOptions(interval = 250))
-    ),
-    mainPanel(
-      leafletOutput("map", height = "600px", width = "800px"),
-      plotOutput("plot")
+  # titlePanel("Explore U.S. Satellite Reconnaissance of Nuclear Targets from 1941-1985"),
+  # fluidRow(
+  #   h3("Explore U.S. Satellite Reconnaissance of Nuclear Targets from 1941-1985", class = "subtitle"),
+  #   br()
+  # ),
+  tabPanel("Map",
+    sidebarLayout(
+      sidebarPanel(HTML("<strong>Filter Facilities and Missiles</strong>"),
+        checkboxInput("showUnknown","Show Facilities with Unknown Start Dates", value = FALSE),
+        checkboxInput("showCaptures","Show Spotted Facilities and Missiles", value = FALSE),
+        sliderInput("dateSlider", "Adjust Date:", 
+                    min = min_date, 
+                    max = max_date, 
+                    value = min_date, 
+                    step = 49)
+                    # animate = animationOptions(interval = 250))
+      ),
+      mainPanel(
+        leafletOutput("map1", height = "600px", width = "800px")
+      )
     )
-  )
+  ),
+  
+  tabPanel("Search",
+       sidebarLayout(
+           sidebarPanel(
+             # ngens: number slider
+             selectInput(inputId = "facility", 
+                         label = "Select a facility:", 
+                         choices = unique(facs$facility_name)),
+             actionButton(inputId = "search",
+                          label = "Search")
+           ), 
+           mainPanel(
+             column(6,
+                uiOutput(outputId = "fac_name"),
+                uiOutput(outputId = "map2label"),
+                leafletOutput("map2", height = "300px", width = "400px"),
+                # uiOutput(outputId = "capture_plot_label"),
+                # plotOutput(outputId = "capture_plot")
+              ), 
+             column(6,
+                uiOutput(outputId = "capture_list")
+             )
+           )
+       )
+    )
 )
 
 
 
 
 
-##SERVER
+##  ------------------------------- SERVER ---------------------------------- ##
 
 server <- function(input, output, session) {
   state <- reactiveValues(fac = facs,
                           miss = missiles)
   
+  # -------------------------------- UTILS -------------------------------------
   # handle unknown dates
   adjust_nas <- function() {
     state$fac <- facs
@@ -118,11 +145,6 @@ server <- function(input, output, session) {
         pic_url <- ""
         
         if(caps$`Data Source`[i] == "declass1") {
-          # pic_url <- paste("https://ims.cr.usgs.gov/browse/DIT/", 
-          #                  caps$Mission[i], "/", 
-          #                  caps$`Direction Flag`[i], "/", 
-          #                  cam_letter, "/",
-          #                  caps$`Display ID`[i], ".jpg", sep = "")
           pic_url <- paste("https://earthexplorer.usgs.gov/scene/metadata/full/5e839febdccb64b3/",
                            caps$`Display ID`[i], sep = "")
           
@@ -147,10 +169,9 @@ server <- function(input, output, session) {
     popups
   }
   
-  
   # add markers where there should be markers
   create_markers <- function() {
-    leafletProxy(mapId = 'map') %>%
+    leafletProxy(mapId = 'map1') %>%
       clearMarkers() %>%
       addCircleMarkers(data = fac_filtered_by_dates(),
                        lng = ~lng, lat = ~lat,
@@ -161,7 +182,7 @@ server <- function(input, output, session) {
                        fillOpacity = 1,
                        popup = ~paste(facility_name, "<br>",
                                       "Facility Start Date: ", start_date,
-                                      ifelse(input$showCaptures, "<br>Not Yet Photographed", "")),
+                                      ifelse(input$showCaptures, "<br>Not Yet Photographed", ""))
       ) %>%
       addMarkers(data = miss_filtered_by_dates(),
                  lng = ~lng, lat = ~lat,
@@ -172,16 +193,6 @@ server <- function(input, output, session) {
                  popup = ~paste("Missile Site",
                                 "<br>Start Date: ", start_date,
                                 ifelse(input$showCaptures, "<br>Not Yet Photographed", "")))
-      # addCircleMarkers(data = miss_filtered_by_dates(),
-      #                  lng = ~lng, lat = ~lat,
-      #                  radius = 5,
-      #                  weight = 1,
-      #                  color = "yellow",
-      #                  opacity = 1,
-      #                  fillOpacity = 0.5,
-      #                  popup = ~paste("Missile Site",
-      #                                 "<br>Start Date: ", start_date,
-      #                                 ifelse(input$showCaptures, "<br>Not Yet Photographed", "")))
     
     if (input$showCaptures) {
       fac_caps <- captured_facs()
@@ -195,7 +206,7 @@ server <- function(input, output, session) {
       miss_popups <- get_popups(caps = miss_caps, is_facs = FALSE)
       
       
-      leafletProxy(mapId = 'map') %>%
+      leafletProxy(mapId = 'map1') %>%
         addCircleMarkers(data = fac_caps,
                          lng = fac_lngs, lat = fac_lats,
                          radius = 2.8,
@@ -205,15 +216,6 @@ server <- function(input, output, session) {
                          fillOpacity = 1,
                          popup = fac_popups
         ) %>%
-        # addCircleMarkers(data = fac_caps,
-        #                  lng = fac_lngs, lat = fac_lats,
-        #                  radius = 2.8,
-        #                  weight = 1,
-        #                  color = "#dd2119",
-        #                  opacity = 1,
-        #                  fillOpacity = 1,
-        #                  popup = miss_popups
-        # )
         addMarkers(data = miss_caps,
                          lng = miss_lngs, lat = miss_lats,
                          icon = makeIcon(
@@ -225,8 +227,8 @@ server <- function(input, output, session) {
     }
   }
   
-  # run on startup. Adjust NAs and filter by date
-  output$map <- renderLeaflet({
+  # map for Map page. run on startup. Adjust NAs and filter by date
+  output$map1 <- renderLeaflet({
     map <- leaflet(leafletOptions( minZoom = 0 )) %>%
       addTiles() %>%
       setView(lng = 0,
@@ -235,6 +237,19 @@ server <- function(input, output, session) {
     map
   })
   
+  # map for Search page.
+  output$map2 <- renderLeaflet({
+    map <- leaflet(leafletOptions( minZoom = 0 )) %>%
+      addTiles() %>%
+      setView(lng = 0,
+              lat = 0,
+              zoom = 1)
+    map
+  })
+  
+  
+  
+  # --------------------------- EVENT HANDLERS ---------------------------------
   # re-create map whenever date slider is changed
   observeEvent(eventExpr = { input$dateSlider }, handlerExpr = {
     adjust_nas()
@@ -251,6 +266,65 @@ server <- function(input, output, session) {
   observeEvent(eventExpr = { input$showCaptures }, handlerExpr = {
     adjust_nas()
     create_markers()
+  })
+  
+  # search for a facility
+  observeEvent(eventExpr = { input$search }, handlerExpr = {
+    fac_name <-  input$facility
+    facility <- facs %>% filter(facility_name == fac_name)
+    
+    output$fac_name <- renderUI({
+      HTML(paste("<strong>Facility Name:</strong>", fac_name, "<br><br>"))
+    })
+      
+    output$map2label <- renderUI({
+      HTML("<strong>Map:</strong>")})
+    
+    # create map marker, adjust map zoom
+    leafletProxy(mapId = 'map2') %>%
+      clearMarkers() %>%
+      addCircleMarkers(data = facility,
+                       lng = facility$lng, lat = facility$lat,
+                       radius = 2.8,
+                       weight = 1,
+                       color = "#2a297b",
+                       opacity = 1,
+                       fillOpacity = 1
+                       # popup = fac_popups
+      ) %>%
+      setView(lng = facility$lng, lat = facility$lat, zoom = 5)
+    
+    # output$capture_plot_label <- renderUI({
+    #   HTML("<br><strong>Capture Plot: </strong>")
+    # })
+    
+    # # create capture plot
+    # capture_counts <- fac_caps %>%
+    #   mutate(`Abbreviated Mission` = substr(Mission,1,4)) %>%
+    #   filter(facility_name == fac_name) %>%
+    #   group_by(facility_name, `Acquisition Date`, `Abbreviated Mission`, start_date) %>%
+    #   summarise(n_caps = n()) 
+    # 
+    # yearly_counts <- capture_counts %>%
+    #   mutate(`Year` = as.numeric(substr(`Acquisition Date`,1,4))) %>%
+    #   group_by(facility_name, Year) %>%
+    #   summarise(n_caps = sum(n_caps)) %>%
+    #   ungroup()
+    # 
+    # yearly_counts %>% 
+    #   ggplot(aes(x = Year, y = n_caps, group = 1)) +
+    #   geom_line()  
+    # 
+    # output$capture_plot <- renderPlot({
+    #   yearly_counts %>% 
+    #     ggplot(aes(x = Year, y = n_caps, group = 1)) +
+    #     geom_col()  
+    # })
+    
+    output$capture_list <- renderUI({
+      HTML("<br><strong>Capture Plot: </strong>")
+    })
+    
   })
 }
 
